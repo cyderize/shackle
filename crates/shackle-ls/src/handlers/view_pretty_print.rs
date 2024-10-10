0cfc1014f3382beb16ed214b934f70c5af06dab9
Jason N <cyderize@users.noreply.github.com>
2024-10-10 15:20:02 +1100

    Format pretty printed THIR language server handler

--- crates/shackle-ls/src/handlers/view_pretty_print.rs
+++ crates/shackle-ls/src/handlers/view_pretty_print.rs
@@ -1,11 +1,15 @@
+use std::sync::Arc;
+
 use lsp_server::ResponseError;
 use lsp_types::TextDocumentPositionParams;
 use shackle_compiler::{
-	db::CompilerDatabase,
-	file::ModelRef,
+	db::{CompilerDatabase, FileReader, Inputs},
+	file::{InputFile, InputLang, ModelRef},
 	hir::db::Hir,
+	syntax::{ast::ConstraintModel, db::SourceParser},
 	thir::{db::Thir, pretty_print::PrettyPrinter},
 };
+use shackle_fmt::{format_model, MiniZincFormatOptions};
 
 use crate::{db::LanguageServerContext, dispatch::RequestHandler, extensions::ViewPrettyPrint};
 
@@ -28,7 +32,20 @@ impl RequestHandler<ViewPrettyPrint, ModelRef> for ViewPrettyPrintHandler {
 				Err(e) => return Ok(format!("%: THIR error: {}", e)),
 			};
 			let printer = PrettyPrinter::new(db, &thir);
-			Ok(printer.pretty_print())
+			let text = printer.pretty_print();
+			let mut c = CompilerDatabase::default();
+			c.set_input_files(Arc::new(vec![InputFile::String(text, InputLang::MiniZinc)]));
+			match c.ast(c.input_file_refs()[0]) {
+				Ok(ConstraintModel::MznModel(model)) => {
+					let formatted = format_model(&model, &MiniZincFormatOptions::default());
+					if let Some(f) = formatted {
+						return Ok(f);
+					}
+				}
+				Err(_) => (),
+				_ => unreachable!(),
+			}
+			Ok("% Failed to format parsed text".to_owned())
 		} else {
 			Ok("% Errors present.".to_owned())
 		}

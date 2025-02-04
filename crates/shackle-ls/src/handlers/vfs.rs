27f4819dcf0756761dd803fdb2c45d9da507f760
Jason N <cyderize@users.noreply.github.com>
2025-02-04 11:21:00 +1100

    Fix path handling in language server

--- crates/shackle-ls/src/handlers/vfs.rs
+++ crates/shackle-ls/src/handlers/vfs.rs
@@ -1,20 +1,18 @@
-use std::path::Path;
-
 use lsp_types::{
 	DidChangeTextDocumentParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams,
 };
 
-use crate::LanguageServerDatabase;
+use crate::{utils::uri_to_path, LanguageServerDatabase};
 
 pub fn on_document_open(db: &mut LanguageServerDatabase, params: DidOpenTextDocumentParams) {
-	let file = params.text_document.uri.as_str();
-	db.manage_file(Path::new(file), &params.text_document.text);
+	let file = uri_to_path(&params.text_document.uri);
+	db.manage_file(&file, &params.text_document.text);
 }
 
 pub fn on_document_changed(db: &mut LanguageServerDatabase, params: DidChangeTextDocumentParams) {
-	let file = params.text_document.uri.as_str();
+	let file = uri_to_path(&params.text_document.uri);
 	db.manage_file(
-		Path::new(file),
+		&file,
 		&params
 			.content_changes
 			.iter()
@@ -24,6 +22,6 @@ pub fn on_document_changed(db: &mut LanguageServerDatabase, params: DidChangeTex
 }
 
 pub fn on_document_closed(db: &mut LanguageServerDatabase, params: DidCloseTextDocumentParams) {
-	let file = params.text_document.uri.as_str();
-	db.unmanage_file(Path::new(file));
+	let file = uri_to_path(&params.text_document.uri);
+	db.unmanage_file(&file);
 }
